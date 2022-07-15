0ef4a2958ec390770dd8a22c7613ff488db8696e
josaph17 <say.dashiev@gmail.com>
2022-07-15 21:49:21 +0800

    Add Range project

--- Range/src/ru/academits/dashiev/range/Range.java
+++ Range/src/ru/academits/dashiev/range/Range.java
@@ -1,4 +1,38 @@
 package ru.academits.dashiev.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double point) {
+        double epsilon = 1.0e-10;
+
+        return (point - from >= -epsilon && to - point >= -epsilon);
+    }
 }
+
--- Range/src/ru/academits/dashiev/range_main/Main.java
+++ Range/src/ru/academits/dashiev/range_main/Main.java
@@ -1,7 +1,34 @@
 package ru.academits.dashiev.range_main;
+import ru.academits.dashiev.range.Range;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Range range = new Range(11.3, 32.4);
+
+        double point = 32.6;
+
+        double length = range.getLength();
+        System.out.printf("Длина диапазона от %.1f до %.1f равна %.1f%n", range.getFrom(), range.getTo(), length);
+
+        if (range.isInside(point)) {
+            System.out.printf("Число %.1f принадлежит диапазону ", point);
+        } else {
+            System.out.printf("Число %.1f не принадлежит диапазону от %.1f до %.1f%n", point, range.getFrom(), range.getTo());
+        }
+
+        System.out.println();
+
+        range.setFrom(32.5);
+        range.setTo(32.7);
+
+        length = range.getLength();
+        System.out.printf("Длина диапазона от %.1f до %.1f равна %.1f%n", range.getFrom(), range.getTo(), length);
+
+        if (range.isInside(point)) {
+            System.out.printf("Число %.1f принадлежит диапазону от %.1f до %.1f%n", point, range.getFrom(), range.getTo());
+        } else {
+            System.out.printf("Число %.1f не принадлежит диапазону от %.1f до %.1f%n", point, range.getFrom(), range.getTo());
+        }
     }
 }
+
