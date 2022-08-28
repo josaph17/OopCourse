21f208c7bf313bc2de18a4c245593b8d18837226
josaph17 <say.dashiev@gmail.com>
2022-08-28 19:18:52 +0800

    fixed

--- Range/src/ru/academits/dashiev/range_main/Main.java
+++ Range/src/ru/academits/dashiev/range_main/Main.java
@@ -51,8 +51,8 @@ public class Main {
         checkRangesOperations(6, 8.9, 4.4, 6.3); // перес в двух точках
         checkRangesOperations(5.2, 8.3, 7.8, 10); // перес в конце this
         checkRangesOperations(6.7, 10, 7.2, 8.8); // второй отрезок внутри this
-        checkRangesOperations(10, 10.5, 9.9, 11.9); // this внутри второго отрещка
-        checkRangesOperations(5, 6.6, 6.6, 7.8); // this внутри второго отрещка
-        checkRangesOperations(5.5, 10.2, 4.4, 20); // this внутри второго отрещка
+        checkRangesOperations(10, 10.5, 9.9, 11.9); // this внутри второго отрезка
+        checkRangesOperations(5, 6.6, 6.6, 7.8); // this внутри второго отрезка
+        checkRangesOperations(5.5, 10.2, 4.4, 20); // this внутри второго отрезка
     }
 }
