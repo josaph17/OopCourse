66031a4ad98a86b0c270e293ffdee5a726e92a11
Sayan <say.dashiev@gmail.com>
2022-10-08 14:36:04 +0800

    List refactor 1

--- List/src/ru/academits/dashiev/list/List.java
+++ List/src/ru/academits/dashiev/list/List.java
@@ -1,8 +1,10 @@
 package ru.academits.dashiev.list;
 
+import java.util.Arrays;
+
 public class List<T> {
     private T[] items;
-    private int size; // вместимость списка , длина массива и вместимость списка могут отличаться
+    private int size; // 0, по умолчанию , вместимость списка , длина массива и вместимость списка могут отличаться
 
     public List() {
         items = (T[]) new Object[10];
@@ -10,26 +12,50 @@ public class List<T> {
     }
 
     public int size() { // получение размера списка
-
+        return size; // размер списка называют вместимостью, capacity
     }
 
     public T getHead() { // получение значения первого элемента
-
+        return  items[0];
     }
 
     public T getItem(int index) { // получение значения по указанному индексу
-        //TODO бросить исключение если выход за size
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "IndexOutOfBoundsException. List capacity = " + size + ".Current value = " + index); // выход за sizes
+        }
 
+        return items[index];
     }
 
-    public void setItem(int index, T listItem) {
-        // изменение значения по указанному индексу, Изменение значения по индексу пусть выдает старое значение.
-        //TODO бросить исключение если выход за size
+    public void increaseCapacity() {
+        items = Arrays.copyOf(items, items.length * 2);
+    }
 
+    public T setItem(int index, T listItem) {
+        // изменение значения по указанному индексу, Изменение значения по индексу пусть выдает старое значение.
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "IndexOutOfBoundsException. List capacity = " + size + ".Current value = " + index); // выход за sizes
+        }
+
+        if (size >= items.length) {
+            increaseCapacity();
+        }
+
+        T oldValue  = items[index];
+
+        items[index] = listItem;
+
+        return oldValue;
     }
 
     public void remove(int index) { // удаление элемента по индексу, пусть выдает значение элемента
-        //TODO бросить исключение если выход за size
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "IndexOutOfBoundsException. List capacity = " + size + ".Current value = " + index); // выход за sizes
+        }
+
 
     }
 
@@ -38,7 +64,10 @@ public class List<T> {
     }
 
     public void addIndexItem(int index, T item) { //вставка элемента по индексу
-        //TODO бросить исключение если выход за size
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "IndexOutOfBoundsException. List capacity = " + size + ".Current value = " + index); // выход за sizes
+        }
 
     }
 
