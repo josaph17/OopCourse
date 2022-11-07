9e98b5c8c68c8eacd743cd50b15ad16f0bb7d619
Sayan <say.dashiev@gmail.com>
2022-11-08 00:17:35 +0800

    Hello

--- Lambda/src/ru/academits/dashiev/lambda_main/Main.java
+++ Lambda/src/ru/academits/dashiev/lambda_main/Main.java
@@ -4,29 +4,37 @@ import ru.academits.dashiev.person.Person;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new LinkedList<>();
-        people.add(new Person("Иван", 33));
+        people.add(new Person("Иван", 23));
         people.add(new Person("Сергей", 27));
-        people.add(new Person("Петр", 15));
+        people.add(new Person("Петр", 16));
         people.add(new Person("Лиза", 20));
         people.add(new Person("Сергей", 15));
         people.add(new Person("Анна", 21));
         people.add(new Person("Джон", 23));
         people.add(new Person("Джон", 9)); // 8 человек
 
-        List<String> list1 = people.stream().map(x -> x.getName()).distinct().collect(
+        List<String> uniqueNames = people.stream().map(x -> x.getName()).distinct().collect(
                 Collectors.toList());
 
-        System.out.println("Имена: " + list1.stream().collect(Collectors.joining(", ")));
+        System.out.println("Имена: " + uniqueNames.stream().collect(Collectors.joining(", ")));
 
         List<Person> list2 = people.stream().filter(x -> x.getAge() < 18).collect(Collectors.toList());
         System.out.println(list2);
 
-        Stream<Integer> stream2 = list2.stream().reduce(0, (memo, e)-> memo+e);
+        list2.stream()
+                .mapToInt(x-> x.getAge())
+                .average()
+                .stream()
+                .mapToInt(x->(int)x)
+                .findFirst()
+                .ifPresent(System.out::println);
     }
 }
