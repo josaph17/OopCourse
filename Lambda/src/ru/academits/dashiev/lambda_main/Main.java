a2010bfdc678bfcd329e7dd6a75c5222d9c8d0b5
Sayan <say.dashiev@gmail.com>
2022-11-08 14:12:04 +0800

    Lambda.1 ver 1

--- Lambda/src/ru/academits/dashiev/lambda_main/Main.java
+++ Lambda/src/ru/academits/dashiev/lambda_main/Main.java
@@ -21,20 +21,36 @@ public class Main {
         people.add(new Person("Джон", 23));
         people.add(new Person("Джон", 9)); // 8 человек
 
+        // А) получить список униковльных имен
         List<String> uniqueNames = people.stream().map(x -> x.getName()).distinct().collect(
                 Collectors.toList());
 
+        // Б) вывести список уникальных имен
         System.out.println("Имена: " + uniqueNames.stream().collect(Collectors.joining(", ")));
 
-        List<Person> list2 = people.stream().filter(x -> x.getAge() < 18).collect(Collectors.toList());
-        System.out.println(list2);
+        // В) получить список людей младше 18 и посчитать для них средний возраст
+        List<Person> peopleYounger18 = people.stream()
+                .filter(x -> x.getAge() < 18)
+                .collect(Collectors.toList());
 
-        list2.stream()
+        System.out.println("Младше 18: " + peopleYounger18);
+
+        peopleYounger18.stream()
                 .mapToInt(x-> x.getAge())
                 .average()
                 .stream()
                 .mapToInt(x->(int)x)
                 .findFirst()
-                .ifPresent(System.out::println);
+                .ifPresent(x->System.out.println("Средний возраст людей до 18 лет: " + x));
+
+        // Г) при помощи группировки получить Map, в котором ключи - имена, а значения - средний возраст
+
+        // Д) от 20 до 45
+        Stream<String> peopleFrom18To45 = people.stream()
+                        .filter(x-> x.getAge()>=20 && x.getAge()<=45 )
+                        .sorted((p1,p2)->p2.getAge()-p1.getAge())
+                        .map(x->x.getName());
+
+        System.out.println("От 20 до 45: " + peopleFrom18To45.collect(Collectors.joining(", ")));
     }
 }
