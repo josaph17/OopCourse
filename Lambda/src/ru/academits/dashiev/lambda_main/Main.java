1417e69c6a48c0c7018eae2c392f3d7605a90457
Sayan <say.dashiev@gmail.com>
2022-11-09 12:36:29 +0800

    Lambda1.  ver 2

--- Lambda/src/ru/academits/dashiev/lambda_main/Main.java
+++ Lambda/src/ru/academits/dashiev/lambda_main/Main.java
@@ -2,55 +2,69 @@ package ru.academits.dashiev.lambda_main;
 
 import ru.academits.dashiev.person.Person;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> people = new LinkedList<>();
-        people.add(new Person("Иван", 23));
-        people.add(new Person("Сергей", 27));
-        people.add(new Person("Петр", 16));
-        people.add(new Person("Лиза", 20));
-        people.add(new Person("Сергей", 15));
-        people.add(new Person("Анна", 21));
-        people.add(new Person("Джон", 23));
-        people.add(new Person("Джон", 9)); // 8 человек
+        List<Person> persons = new LinkedList<>();
+        persons.add(new Person("Иван", 23));
+        persons.add(new Person("Сергей", 44));
+        persons.add(new Person("Джон", 16));
+        persons.add(new Person("Лиза", 20));
+        persons.add(new Person("Сергей", 15));
+        persons.add(new Person("Анна", 21));
+        persons.add(new Person("Джон", 33));
+        persons.add(new Person("Джон", 10)); // 8 человек
+
+        System.out.println("Изначальный список: ");
+        persons.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+        System.out.println("--------------");
 
         // А) получить список униковльных имен
-        List<String> uniqueNames = people.stream().map(x -> x.getName()).distinct().collect(
+        List<String> uniqueNamesPersonsList = persons.stream().map(x -> x.getName()).distinct().collect(
                 Collectors.toList());
 
         // Б) вывести список уникальных имен
-        System.out.println("Имена: " + uniqueNames.stream().collect(Collectors.joining(", ")));
+        System.out.println("Б) Имена: " + uniqueNamesPersonsList.stream().collect(Collectors.joining(", ")));
 
         // В) получить список людей младше 18 и посчитать для них средний возраст
-        List<Person> peopleYounger18 = people.stream()
+        List<Person> younger18PersonsList = persons.stream()
                 .filter(x -> x.getAge() < 18)
                 .collect(Collectors.toList());
 
-        System.out.println("Младше 18: " + peopleYounger18);
+        System.out.println("В) List людей младше 18: " + younger18PersonsList);
 
-        peopleYounger18.stream()
+        younger18PersonsList.stream()
                 .mapToInt(x-> x.getAge())
                 .average()
                 .stream()
                 .mapToInt(x->(int)x)
                 .findFirst()
-                .ifPresent(x->System.out.println("Средний возраст людей до 18 лет: " + x));
+                .ifPresent(x->System.out.println("Средний возраст людей младше 18: " + x));
 
         // Г) при помощи группировки получить Map, в котором ключи - имена, а значения - средний возраст
 
-        // Д) от 20 до 45
-        Stream<String> peopleFrom18To45 = people.stream()
-                        .filter(x-> x.getAge()>=20 && x.getAge()<=45 )
-                        .sorted((p1,p2)->p2.getAge()-p1.getAge())
-                        .map(x->x.getName());
+        /* Map<String, List<Person>> neoPeople = people.stream().collect(Collectors.groupingBy(p-> p.getName());
+         -- прошу не считать за ошибку, Приготитсяздать Map, где ключ - имя, значение List<Person>, зо*/
+        /* Map<Integer, List<Person>> peopleByAge = people.stream()
+                .collect(Collectors.groupingBy(Person::getAge));
+                -- прошу не считать за ошибку, создать Map, где ключ - возраст*/
 
-        System.out.println("От 20 до 45: " + peopleFrom18To45.collect(Collectors.joining(", ")));
+        Map<String, Double> personsMap = persons.stream()
+               .collect(Collectors.groupingBy(p->p.getName(), Collectors.averagingInt(p->p.getAge())));
+
+        System.out.println("Г) peopleMap: " + personsMap);
+
+        // Д) от 20 до 45
+        String from20To45Persons =  persons.stream()
+                .filter(x-> x.getAge()>=20 && x.getAge()<=45 )
+                .sorted((p1,p2)->p2.getAge()-p1.getAge())
+                .map(x->x.getName())
+                .collect(Collectors.joining(", "));
+
+        System.out.println("Д) Люди от 20 до 45: " + from20To45Persons);
     }
 }
