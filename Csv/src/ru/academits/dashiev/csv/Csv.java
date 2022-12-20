0bcb3def0967272b5c115fa33c07deadfb609afe
Sayan <say.dashiev@gmail.com>
2022-12-20 16:53:14 +0800

    Refactoring in the "CSV" project.

--- Csv/src/ru/academits/dashiev/csv/Csv.java
+++ Csv/src/ru/academits/dashiev/csv/Csv.java
@@ -1,4 +1,32 @@
 package ru.academits.dashiev.csv;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class Csv {
+    public static void main(String[] args) throws IOException {
+        // Буфферизованные потоки
+        String readLine = null;
+
+        String tableStart = "<table>";
+        String tableEnd = "<table>";
+
+        try (BufferedReader reader = new BufferedReader(
+                // не получается поставить StandardCharsets.UTF_8
+                new FileReader("csv.txt", StandardCharsets.UTF_8));
+             BufferedWriter writer = new BufferedWriter(
+                new FileWriter("csv2.txt", StandardCharsets.UTF_8))) {
+            while ((readLine = reader.readLine()) != null) {
+                writer.write(tableStart);
+
+                System.out.print(readLine);
+                writer.write(readLine);
+
+                writer.write(tableEnd);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Csv/src/ru/academits/dashiev/csv_main/Main.java
+++
@@ -1,6 +0,0 @@
-package ru.academits.dashiev.csv_main;
-
-import ru.academits.dashiev.csv.Csv;
-
-public class Main {
-}
