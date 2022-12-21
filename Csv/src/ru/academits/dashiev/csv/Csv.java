26d6a4a42b6818cefcee19e056a01a638aa585ac
Sayan <say.dashiev@gmail.com>
2022-12-21 10:56:56 +0800

    Refactoring in the "CSV " project. Create carcas for program

--- Csv/src/ru/academits/dashiev/csv/Csv.java
+++ Csv/src/ru/academits/dashiev/csv/Csv.java
@@ -1,29 +1,38 @@
 package ru.academits.dashiev.csv;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Csv {
     public static void main(String[] args) throws IOException {
         // Буфферизованные потоки
-        String readLine = null;
+        int readLine;
 
-        String tableStart = "<table>";
-        String tableEnd = "<table>";
+        String sourcePath = "csv.txt";
+        String resultPath = "csv2.html";
+        String tagTableStart = "<table>";
+        String tagTableEnd = "</table>";
+        String tagTrStart = "<tr>"; // tableRow
+        String tagTrEnd = "</tr>";
+        String tagTdStart = "<td>"; // tableDetail
+        String tagTdEnd = "</td>";
+        String tagBr = "<br>"; // перевод сроки
+        String symbolLess = "&lt"; // symbol <
+        String symbolBigger = "&gt"; // symbol >
+        String symbolAmp = "&amp"; // symbol &
 
-        try (BufferedReader reader = new BufferedReader(
-                // не получается поставить StandardCharsets.UTF_8
-                new FileReader("csv.txt", StandardCharsets.UTF_8));
-             BufferedWriter writer = new BufferedWriter(
-                new FileWriter("csv2.txt", StandardCharsets.UTF_8))) {
-            while ((readLine = reader.readLine()) != null) {
-                writer.write(tableStart);
+        char[] buffer = new char[100];
 
-                System.out.print(readLine);
-                writer.write(readLine);
+        try (BufferedReader reader = //
+                     new BufferedReader( //
+                             new FileReader(sourcePath, StandardCharsets.UTF_8));
+             BufferedWriter writer = //
+                     new BufferedWriter( //
+                             new FileWriter(resultPath, StandardCharsets.UTF_8))) { //
 
-                writer.write(tableEnd);
+            while ((readLine = reader.read()) != -1) {
+                System.out.print((char) readLine);
+                writer.write((char) readLine);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
