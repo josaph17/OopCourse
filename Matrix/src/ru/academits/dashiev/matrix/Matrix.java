c0b10cc096244caa52d7e767470f113741936c1a
Sayan <say.dashiev@gmail.com>
2022-12-31 10:42:37 +0800

    Refactoring in the "Matrix" project.
    create constructors

--- Matrix/src/ru/academits/dashiev/matrix/Matrix.java
+++ Matrix/src/ru/academits/dashiev/matrix/Matrix.java
@@ -1,2 +1,133 @@
-package ru.academits.dashiev.matrix;public class Matrix {
+package ru.academits.dashiev.matrix;
+
+import ru.academits.dashiev.vector.Vector;
+
+import java.util.Arrays;
+
+public class Matrix {
+    private int rows;
+    private int cols; // размерность вектора
+
+    private Vector[] vectors;
+
+    public Matrix(int n, int m) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Matrix rows must be > 0. Current rows = " + n);
+        }
+
+        if (m <= 0) {
+            throw new IllegalArgumentException("Matrix cols must be > 0. Current cols = " + m);
+        }
+
+        rows = n;
+        cols = m;
+
+        vectors = new Vector[n]; // обязательно проинициализируем vectors
+
+        for (int i = 0; i < n; i++) {
+            vectors[i] = new Vector(m);
+        }
+    }
+
+    public Matrix(Matrix matrix) {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix is null");
+        }
+
+        rows = matrix.rows;
+        cols = matrix.cols;
+
+        vectors = new Vector[rows]; // создаем массив векторов
+
+        for (int i = 0; i < rows; i++) {
+            vectors[i] = new Vector(matrix.vectors[i]);
+        }
+    }
+
+    public Matrix(double[][] array) {
+        if (array == null) {
+            throw new NullPointerException("Array is null");
+        }
+
+        rows = array.length;
+
+        vectors = new Vector[rows];
+
+        // Находим кол-во столбцов матрицы
+        for (int i = 0; i < rows; i++) {
+            int currentCapacity = array[i].length;
+
+            if (currentCapacity > cols) {
+                cols = currentCapacity;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            vectors[i] = new Vector(Arrays.copyOf(array[i], cols));
+        }
+    }
+
+    public Matrix(Vector[] vector) {
+        if (vector == null) {
+            throw new NullPointerException("Vector is null");
+        }
+
+        rows = vector.length;
+
+        vectors = new Vector[vector.length];
+
+        // Находим максимальную длину подВектора
+        for (int i = 0; i < rows; i++) {
+            int currentCapacity = vector[i].getSize();
+
+            if (currentCapacity > cols) {
+                cols = currentCapacity;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            vectors[i] = new Vector(cols);
+
+            for (int j = 0; j < vector[i].getSize(); j++) {
+                vectors[i].setComponent(j, vector[i].getComponent(j));
+            }
+        }
+    }
+
+    public int rows() {
+        return rows;
+    }
+
+    public int cols() {
+        return cols;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= rows) {
+            throw new IndexOutOfBoundsException(
+                    "Matrix min rowIndex = 0, max index = " + (rows - 1) + ". Current rowIndex = " + index);
+        }
+    }
+
+    // c.Получение вектора - столбца по индексу
+    // d.Транспонирование матрицы
+    // e.Умножение на скаляр
+    // f.Вычисление определителя матрицы
+    // g.toString определить так, чтобы результат получался в таком виде: { { 1, 2 }, { 2, 3 } }
+    // h.Умножение матрицы на вектор
+    // i.Сложение матриц
+    // j.Вычитание матриц
+    // Статические методы. a.Сложение матриц
+    // Статические методы. b.Вычитание матриц
+    // Статические методы. c.Умножение матриц
+
+    // a.Получение размеров матрицы
+    public int size() {
+        return rows * cols;
+    }
+
+    // b.Получение и задание вектора-строки по индексу
+    public Vector getRow(int index) {
+        return null;
+    }
 }
--- Matrix/src/ru/academits/dashiev/matrix_main/Main.java
+++ Matrix/src/ru/academits/dashiev/matrix_main/Main.java
@@ -1,2 +1,33 @@
-package ru.academits.dashiev.matrix_main;public class Main {
+package ru.academits.dashiev.matrix_main;
+
+import ru.academits.dashiev.matrix.Matrix;
+import ru.academits.dashiev.vector.Vector;
+
+public class Main {
+    public static void checkConstructors() {
+        //        double[][] test = {{0.9, 2.1, 5.2, 4.2}, {3, 42.7, 6.5}, {321, 7.1, 43.9, 111.3}};
+        //
+        //        Matrix matrix = new Matrix(test);
+        //
+        //        System.out.println(test.length);
+        //        System.out.println(matrix.cols);
+
+        double[] array1 = {21.3, 43.4};
+        double[] array2 = {9};
+        double[] array3 = {342.9, 7.05, 111, 32132.43, 27};
+
+        Vector vector1 = new Vector(array1);
+        Vector vector2 = new Vector(array2);
+        Vector vector3 = new Vector(array3);
+
+        Vector[] vectorsArray = {vector1, vector2, vector3};
+
+        Matrix vectorsMatrix = new Matrix(vectorsArray);
+
+        System.out.println("Whats  all!");
+    }
+
+    public static void main(String[] args) {
+        checkConstructors();
+    }
 }
