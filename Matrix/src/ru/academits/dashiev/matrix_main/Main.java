ec9cc3442f20f527d761320a82406ea10b5ad3b4
Sayan <say.dashiev@gmail.com>
2023-01-16 16:54:35 +0800

    Refactoring in the "Matrix" project.

--- Matrix/src/ru/academits/dashiev/matrix_main/Main.java
+++ Matrix/src/ru/academits/dashiev/matrix_main/Main.java
@@ -26,51 +26,38 @@ public class Main {
     }
 
     public static void checkMethods() {
-        double[] array1 = {21.3, 43.4};
-        double[] array2 = {9};
-        double[] array3 = {342.9, 7.05, 111, 9.43, 27};
+        double[] array1 = {21.3, 43.4, 3.2, 6};
+        double[] array2 = {9, 9.17, 1.62, 10};
+        double[] array3 = {5.9, 7.05, 8};
+        double[] array4 = {0, 1.9, 4.4, 2.28};
 
         Vector vector1 = new Vector(array1);
         Vector vector2 = new Vector(array2);
         Vector vector3 = new Vector(array3);
+        Vector vector4 = new Vector(array4);
 
-        Vector[] vectorsArray = {vector1, vector2, vector3};
+        Vector[] vectorsArray = {vector1, vector2, vector3, vector4};
 
         Matrix vectorsMatrix = new Matrix(vectorsArray);
 
-        //System.out.println(vectorsMatrix.getRow(3));
-
-        double[] inArray = {3.4, 1111.1, 21.3, 342.4, 565};
-        vectorsMatrix.setVectorRow(0, inArray);
+        //double[] inArray = {3.4, 1111.1, 21.3, 342.4, 565};
+        //vectorsMatrix.setVectorRow(0, inArray);
         // System.out.println(vectorsMatrix.getVectorRow(0));
 
         System.out.println("Before transpose");
 
         System.out.println(vectorsMatrix);
 
-        vectorsMatrix.transpose();
-
-        System.out.println("After transpose");
-
-        System.out.println(vectorsMatrix);
-    }
-
-    public static void main(String[] args) {
-        // checkMethods();
-        double[] array1 = {21.3, 43.4};
-        double[] array2 = {9, 9.17, 1.62};
-        double[] array3 = {5.9, 7.05, 8};
-
-        Vector vector1 = new Vector(array1);
-        Vector vector2 = new Vector(array2);
-        Vector vector3 = new Vector(array3);
-
-        Vector[] vectorsArray = {vector1, vector2, vector3};
-
-        Matrix vectorsMatrix = new Matrix(vectorsArray);
+        //vectorsMatrix.transpose();
+        //System.out.println("After transpose");
+        //System.out.println(vectorsMatrix);
 
         double det = vectorsMatrix.determinant(vectorsMatrix.cols());
 
         System.out.println(det);
+    }
+
+    public static void main(String[] args) {
+        checkMethods();
     }
 }
