cb1b155c28bdaa7e6f55fff011dec2f031e06fa8
Sayan <say.dashiev@gmail.com>
2023-01-26 18:34:29 +0800

    Refactoring in the "Graph" project.

--- Graph/src/ru/academits/dashiev/graph/Graph.java
+++ Graph/src/ru/academits/dashiev/graph/Graph.java
@@ -1,5 +1,6 @@
 package ru.academits.dashiev.graph;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -63,31 +64,24 @@ public class Graph {
 
         boolean[] visited = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
-            visited[i] = false;
-        }
-
         Queue<Integer> queue = new LinkedList<>();
 
         // положить в очередь вершину графа, обычно это 1, false если не получается вставить, добавл в конец
         queue.offer(1);
 
-        while (!queue.isEmpty()) { // пока очередь не пуста
-            // достаем 1-ый элемент из очереди и удаляем его
-            Integer recently = queue.poll();
+        while (!queue.isEmpty()) {
+            // пока очередь не пуста, достаем 1-ый элемент из очереди и удаляем его
+            Integer current = queue.poll();
 
-            if (!visited[recently - 1]){
-                visited[recently - 1] = true;
-
-                System.out.print((recently) + " ");
+            if (!visited[current - 1]) {
+                System.out.print((current) + " ");
+                visited[current - 1] = true;
 
                 for (int i = 0; i < n; i++) {
-                    if (matrix[recently - 1][i] == 1) {
+                    if (matrix[current - 1][i] == 1) {
                         // добавляем элемент в очередь
-                        if(!visited[i]){ // если false
-                            queue.offer(i+1);
-
-                            visited[recently - 1] = true;
+                        if (!visited[i]) { // если false
+                            queue.offer(i + 1);
                         }
                     }
                 }
@@ -95,12 +89,81 @@ public class Graph {
 
             // если очередь пуста, посмотрим остались ли еще непосещенные вершины
             // и добавим в очередь, но тоько по одной, постепенно
-            if (queue.isEmpty()){
-                for(int i = 0; i < n; i ++){
-                    if(!visited[i]){ // если вершина не посещена
-                        queue.offer(i+1);
+            if (queue.isEmpty()) {
+                for (int i = 0; i < n; i++) {
+                    if (!visited[i]) { // если вершина не посещена
+                        queue.offer(i + 1);
                         break;
                     }
+                }
+            }
+        }
+
+        System.out.println();
+    }
+
+    public void deepBypass() {
+        int n = vertexCount; // число вершин
+
+        boolean[] visited = new boolean[n];
+
+        Deque<Integer> stack = new LinkedList<>();
+
+        stack.addLast(1);
+
+        while (!stack.isEmpty()) { // пока очередь не пуста
+            Integer current = stack.removeLast(); // достаем последний элемент из стэка и удаляем его
+
+            if (!visited[current - 1]) {
+                System.out.print(current + " "); //  выводим его
+                visited[current - 1] = true;
+
+                for (int i = n - 1; i > 0; i--) {
+                    if (matrix[current - 1][i] == 1) {
+                        // добавляем элемент в очередь
+                        if (!visited[i]) { // если false
+                            stack.addLast(i + 1);
+                        }
+                    }
+                }
+            }
+
+            if (stack.isEmpty()) {
+                for (int i = 0; i < n; i++) {
+                    if (!visited[i]) { // если вершина не посещена
+                        stack.addLast(i + 1);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println();
+    }
+
+    public void recursionDeepBypass(){
+        int n = vertexCount; // число вершин
+
+        boolean[] visited = new boolean[n];
+
+        for(int i = 0; i < vertexCount; i ++){
+            if (!visited[i]){
+                recursionDeepVisit(i+1, visited);
+            }
+        }
+
+        System.out.println();
+    }
+
+    private void recursionDeepVisit(Integer vertex, boolean[] visited) { // private, чтобы не вызвать извне
+        if (!visited[vertex-1]){
+            System.out.print(vertex + " ");
+            visited[vertex-1] = true;
+
+            for (int i = 0; i < vertexCount; i++) {
+                if (matrix[vertex-1][i] == 1) {
+                    // добавляем элемент
+                    recursionDeepVisit(i+1, visited);
                 }
             }
         }
--- Graph/src/ru/academits/dashiev/graph_main/Main.java
+++ Graph/src/ru/academits/dashiev/graph_main/Main.java
@@ -4,15 +4,24 @@ import ru.academits.dashiev.graph.Graph;
 
 public class Main {
     public static void main(String[] args) {
-        Graph path = new Graph(7);
+        Graph path = new Graph(8);
 
-        path.set(1,2,1);
-        path.set(1,5,1);
-        path.set(2,5,1);
-        path.set(3,5,1);
-        path.set(4,5,1);
-        path.set(6,7,1);
+        path.set(1, 2, 1);
+        path.set(1, 5, 1);
+        path.set(2, 5, 1);
+        path.set(2, 8, 1);
+        path.set(3, 5, 1);
+        path.set(3, 8, 1);
+        path.set(4, 5, 1);
+        path.set(6, 7, 1);
 
+        System.out.println("Width bypass:");
         path.widthBypass();
+
+        System.out.println("Deep bypass:");
+        path.deepBypass();
+
+        System.out.println("Recursion deep bypass:");
+        path.recursionDeepBypass();
     }
 }
