5e0659b4b513e7ea738368dee9b477f08cf5668f
Sayan <say.dashiev@gmail.com>
2023-02-11 13:57:16 +0800

    Refactoring in the "Lambda" project.

--- Lambda/src/ru/academits/dashiev/lambda_main/Main.java
+++ Lambda/src/ru/academits/dashiev/lambda_main/Main.java
@@ -6,8 +6,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
+
+import static java.util.stream.Collectors.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,31 +19,32 @@ public class Main {
                                              new Person("Сергей", 15), new Person("Анна", 21),
                                              new Person("Джон", 33), new Person("Джон", 10));
 
-        System.out.println("Изначальный список: ");
+        System.out.println("Изначальный список:");
         persons.forEach(person -> System.out.print(person + " "));
         System.out.println();
 
         System.out.println();
 
+        // Person::getName вместо x -> x.getName(), прошу не удалять важный комментарий
         // А) получить список уникальных имен, Б) вывести список уникальных имен
         String uniqueNames = persons.stream()
-                        .map(x -> x.getName())
+                        .map(Person::getName)
                         .distinct()
-                        .collect(Collectors.joining(", "));
+                        .collect(joining(", "));
 
-        System.out.println("А), Б) список уникальных имен: " + uniqueNames);
+        System.out.println("А), Б) список уникальных имен:" + uniqueNames);
 
         // В) получить список людей младше 18 и посчитать для них средний возраст
         List<Person> younger18PersonsList = persons.stream()
                 .filter(x -> x.getAge() < 18)
-                .collect(Collectors.toList());
+                .toList();
 
         if(younger18PersonsList.isEmpty()){
             System.out.println("B) Can't calculate average!");
         } else{
             // 15. после получения среднего не нужно использовать stream
             younger18PersonsList.stream()
-                    .mapToInt(x -> x.getAge())
+                    .mapToInt(Person::getAge)
                     .average()
                     .ifPresent(x -> System.out.println("В) Средний возраст людей младше 18: " + x));
         }
@@ -55,16 +57,17 @@ public class Main {
                 .collect(Collectors.groupingBy(Person::getAge));
                 -- прошу не считать за ошибку, создать Map, где ключ - возраст*/
 
-        Map<String, Double> nameAndAverageAge = persons.stream().collect(Collectors.groupingBy(p -> p.getName(), Collectors.averagingInt(p -> p.getAge())));
+        Map<String, Double> namesAndAverageAges = persons.stream().collect(
+                groupingBy(Person::getName, averagingInt(Person::getAge)));
 
-        System.out.println("Г) nameAndAverageAge map: " + nameAndAverageAge);
+        System.out.println("Г) namesAndAverageAges map: " + namesAndAverageAges);
 
         // Д) от 20 до 45
         String from20To45Persons = persons.stream()
                 .filter(x -> x.getAge() >= 20 && x.getAge() <= 45)
                 .sorted((p1, p2) -> p2.getAge() - p1.getAge())
-                .map(x -> x.getName())
-                .collect(Collectors.joining(", "));
+                .map(Person::getName)
+                .collect(joining(", "));
 
         System.out.println("Д) Люди от 20 до 45:" + from20To45Persons);
 
@@ -72,18 +75,18 @@ public class Main {
 
         System.out.println("-- Задача 2 --");
 
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите число для которого следует вычислить корни: ");
-        double number = scanner1.nextDouble();
+        double number = scanner.nextDouble();
 
         System.out.print("Введите сколько элементов надо вычислить: ");
-        int elementsCount = scanner2.nextInt();
+        int elementsCount = scanner.nextInt();
 
         // Код ниже выполняется в вертикальном порядке
-        DoubleStream numberRoots = DoubleStream.iterate(number, result -> Math.sqrt(result))
-                .map(result -> Math.sqrt(result))
+        // Math::sqrt вместо result -> Math.sqrt(result), прошу не удалять важный комментарий
+        DoubleStream numberRoots = DoubleStream.iterate(number, Math::sqrt)
+                .map(Math::sqrt)
                 .limit(elementsCount);
 
         numberRoots.forEach(System.out::println);
