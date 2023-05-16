ca676929672622233f3e57a11d4d6025ccae2bb6
Sayan <say.dashiev@gmail.com>
2023-05-16 15:18:03 +0800

    Refactoring in the "Lambda" project.

--- Lambda/src/ru/academits/dashiev/lambda_person/Person.java
+++ Lambda/src/ru/academits/dashiev/lambda_person/Person.java
@@ -2,10 +2,9 @@ package ru.academits.dashiev.lambda_person;
 
 public record Person(String name, int age) {
     public Person {
-        if (age <= 0 || age > 86) {
+        if (age <= 0) {
             throw new IllegalArgumentException("Wrong age value. Now age = " + age);
         }
-
     }
 
     @Override
