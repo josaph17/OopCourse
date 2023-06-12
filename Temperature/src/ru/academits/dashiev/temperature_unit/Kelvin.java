06ca2c8391f7060c9964a97e2ca197caf9fc8714
Sayan <say.dashiev@gmail.com>
2023-06-13 04:59:28 +0800

    Refactoring in the "Temperature" project.

--- Temperature/src/ru/academits/dashiev/temperature_unit/Kelvin.java
+++ Temperature/src/ru/academits/dashiev/temperature_unit/Kelvin.java
@@ -1,37 +1,13 @@
 package ru.academits.dashiev.temperature_unit;
 
-public class Kelvin implements ITemperature {
-    public static final String NAME = "KELVIN";
-
-    private Double value;
-
+public class Kelvin implements ITemperatureUnit {
     @Override
-    public String getName() {
-        return NAME;
-    }
-
-    @Override
-    public Double getValue() {
-        return value;
-    }
-
-    @Override
-    public void setValue(Double value) {
-        this.value = value;
-    }
-
-    @Override
-    public Double calculateKelvin() {
-        return value;
-    }
-
-    @Override
-    public Double calculateFahrenheit() {
-        return (value - 273.15) * (9.0 / 5.0) + 32;
-    }
-
-    @Override
-    public Double calculateCelsius() {
+    public Double calculateCelsius(Double value) {
         return value - 273.15;
+    }
+
+    @Override
+    public Double initFromCelsius(Double value) {
+        return value + 273.15;
     }
 }
