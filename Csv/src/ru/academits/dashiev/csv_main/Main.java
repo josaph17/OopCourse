ec101644678401a41a47d7b8b61208a11bbc6f49
josaph17 <say.dashiev@gmail.com>
2024-01-30 07:31:38 -0500

    Refactoring in the "Csv" project.

--- Csv/src/ru/academits/dashiev/csv_main/Main.java
+++ Csv/src/ru/academits/dashiev/csv_main/Main.java
@@ -1,12 +1,26 @@
 package ru.academits.dashiev.csv_main;
 
-import ru.academits.dashiev.csv.Csv;
+import ru.academits.dashiev.csv.Converter;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String sourcePath = "table.csv";
-        String resultPath = "csv2.html";
+        // table.csv, v2.html
+        try{
+            String[] paths = new String[args.length];
 
-        Csv.readCSV(sourcePath, resultPath);
+            paths[0] = args[0];
+            paths[1] = args[1];
+
+            // System.arraycopy(args, 0, paths, 0, args.length);
+
+            Converter.convertFromCsvToHtml(paths[0], paths[1]);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
