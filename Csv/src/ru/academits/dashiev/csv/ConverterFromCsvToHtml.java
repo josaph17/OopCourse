a105aebcd5b70eacf90c44c386ef4854144c6c2a
josaph17 <say.dashiev@gmail.com>
2024-02-07 00:45:11 -0500

    Refactoring in the "Csv" project.

--- Csv/src/ru/academits/dashiev/csv/ConverterFromCsvToHtml.java
+++ Csv/src/ru/academits/dashiev/csv/ConverterFromCsvToHtml.java
@@ -27,54 +27,54 @@ public class ConverterFromCsvToHtml {
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()) { // Обрабатываем пустую строку
                     writer.print("");
-                }
-
-                if (!isCellInQuotes) {
-                    writer.println("\t\t<tr>");
-                    writer.print("\t\t\t<td>");
                 } else {
-                    writer.print("<br>");
-                }
-
-                for (int i = 0; i < line.length(); i++) {
-                    char currentChar = line.charAt(i);
-
-                    if (currentChar == '"') {
-                        if (!isQuotesFirstTime) {
-                            isQuotesFirstTime = true;
-                            isCellInQuotes = true; // Начинаем включать в кавычки "
-                        } else if (i < (line.length() - 1) && (line.charAt(i + 1) == '"')) {
-                            i++;
-
-                            writer.print('"');
-                        } else { // Если след за "char не равен"
-                            isCellInQuotes = false;
-                            isQuotesFirstTime = false;
-                            // Здесь мы закрываем контент
-                        }
-                    } else if (currentChar == ',') {
-                        if (isCellInQuotes) {
-                            writer.print(currentChar);
-                        } else {
-                            writer.println("</td>");
-                            writer.print("\t\t\t<td>");
-                        }
-                    } else if (currentChar == '<') {
-                        writer.print("&lt;");
-                    } else if (currentChar == '>') {
-                        writer.print("&gt;");
-                    } else if (currentChar == '&') {
-                        writer.print("&amp;");
+                    if (!isCellInQuotes) {
+                        writer.println("\t\t<tr>");
+                        writer.print("\t\t\t<td>");
                     } else {
-                        writer.print(currentChar);
+                        writer.print("<br>");
                     }
-                }
 
-                if (!isCellInQuotes) {
-                    writer.println("</td>");
-                    writer.println("\t\t</tr>");
+                    for (int i = 0; i < line.length(); i++) {
+                        char currentChar = line.charAt(i);
+
+                        if (currentChar == '"') {
+                            if (!isQuotesFirstTime) {
+                                isQuotesFirstTime = true;
+                                isCellInQuotes = true; // Начинаем включать в кавычки "
+                            } else if (i < (line.length() - 1) && (line.charAt(i + 1) == '"')) {
+                                i++;
+
+                                writer.print('"');
+                            } else { // Если след за "char не равен"
+                                isCellInQuotes = false;
+                                isQuotesFirstTime = false;
+                                // Здесь мы закрываем контент
+                            }
+                        } else if (currentChar == ',') {
+                            if (isCellInQuotes) {
+                                writer.print(currentChar);
+                            } else {
+                                writer.println("</td>");
+                                writer.print("\t\t\t<td>");
+                            }
+                        } else if (currentChar == '<') {
+                            writer.print("&lt;");
+                        } else if (currentChar == '>') {
+                            writer.print("&gt;");
+                        } else if (currentChar == '&') {
+                            writer.print("&amp;");
+                        } else {
+                            writer.print(currentChar);
+                        }
+                    }
+
+                    if (!isCellInQuotes) {
+                        writer.println("</td>");
+                        writer.println("\t\t</tr>");
+                    }
+                    // К этому моменту заполнили строчку
                 }
-                // К этому моменту заполнили строчку
             }
 
             writer.println("\t</table>");
