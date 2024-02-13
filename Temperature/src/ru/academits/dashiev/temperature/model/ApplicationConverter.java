c458b6802d698c2c99e96001094fc4b670ff7418
josaph17 <say.dashiev@gmail.com>
2024-02-12 19:50:23 -0500

    Refactoring in the "Temperature" project.

--- Temperature/src/ru/academits/dashiev/temperature/model/ApplicationConverter.java
+++ Temperature/src/ru/academits/dashiev/temperature/model/ApplicationConverter.java
@@ -23,26 +23,36 @@ public class ApplicationConverter implements Converter {
     @Override
     public double convertTemperature(String inputScaleName, String outputScaleName, double inputTemperature) {
         double celsiusTemperature = 0.0;
+        boolean isInputScaleNameFound = false;
 
         for (Scale scale : scales) {
             if (inputScaleName.equals(scale.toString())) {
                 celsiusTemperature = scale.convertToCelsius(inputTemperature);
 
+                isInputScaleNameFound = true;
+
                 break;
             }
+        }
 
+        if (!isInputScaleNameFound){
             throw new IllegalArgumentException("There is no such scale name in all scales list!");
         }
 
         double outputTemperature = 0.0;
+        boolean isOutputScaleNameFound = false;
 
         for (Scale scale : scales) {
             if (outputScaleName.equals(scale.toString())) {
                 outputTemperature = scale.convertFromCelsius(celsiusTemperature);
 
+                isOutputScaleNameFound = true;
+
                 break;
             }
+        }
 
+        if (!isOutputScaleNameFound){
             throw new IllegalArgumentException("There is no such scale name in all scales list!");
         }
 
